e8722102857ea1c84a178a50a3f379a1d0e91a1f
knuser <kcm9453@naver.com>
2024-09-10 16:00:14 +0900

    4FloorChangs
    Signed-off-by: knuser <kcm9453@naver.com>

--- project 2-2,3.py
+++ project 2-2,3.py
@@ -1,5 +1,7 @@
 import numpy as np
-
+import nnfs
+from nnfs.datasets import spiral_data
+import matplotlib.pyplot as plt
 
 class DenseLayer:
     def __init__(self, n_inputs, n_neurons):
@@ -12,7 +14,10 @@ class DenseLayer:
         output = np.maximum(0, output)
         return output
 
-
+nnfs.init()
+X, y = spiral_data(samples=100, classes=2)
+plt.scatter(X[:, 0 ], X[:, 1 ], c = y, cmap = 'brg' )
+plt.show()
 
 n_inputs = 3
 n_neurons = 2
