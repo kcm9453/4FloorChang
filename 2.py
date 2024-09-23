59b3a3f1b1eb0288d00366d8c957cace00f501ca
knuser <kcm9453@naver.com>
2024-09-23 15:28:09 +0900

    4FloorChangs
    Signed-off-by: knuser <kcm9453@naver.com>

--- 2.py
+++ 2.py
@@ -1,7 +1,3 @@
-import random
-
-def init_weights(inputs):
-    weights = []
-    for i in range(len(inputs)):
-            weights.append(random.uniform(-1,1))
-    return weights
+N = int(input())
+for i in range(N):
+    print('*'*(i+1))
